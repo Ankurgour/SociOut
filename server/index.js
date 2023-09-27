f4f0be29a04b6fc1f131b36fc3a286bb0be89b88
Ankur Gour <ankurgour2003@gmail.com>
2023-09-28 01:30:30 +0530

    update strictQuery to false in index file

--- server/index.js
+++ server/index.js
@@ -54,10 +54,11 @@ app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
+mongoose.set('strictQuery', false);
 mongoose
   .connect(process.env.MONGO_URL, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));

608fe440a4293669f903de8ced02379e6de8a495
Ankur Gour <ankurgour2003@gmail.com>
2023-09-30 13:13:31 +0530

    update

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,46 @@
+import { Box, useMediaQuery } from "@mui/material";
+import Navbar from "scenes/navbar";
+import UserWidget from "../widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+
+import { useSelector } from "react-redux";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidgets";
+
+const Homepage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+  
+  return (
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="1rem"
+        justifyContent="space-between"
+      >
+      
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box> 
+         <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget  picturePath={picturePath}/>
+          <PostsWidget userId={_id} isProfile = {false}/>
+        </Box> 
+         {isNonMobileScreens && <Box flexBasis="26%">
+          <AdvertWidget/>
+          <Box m = "2rem 0" />
+          <FriendListWidget userId={_id} />
+         </Box>} 
+      </Box>
+    </Box>
+  );
+};
+export default Homepage;
+//change on 34
---
+++ server/middleware/cloudinaryConfig.js
@@ -0,0 +1,8 @@
+import cloudinary from 'cloudinary';
+
+const cloud = cloudinary.config({
+    cloud_name: 'dvc7w7dra',
+    api_key: '514481745594845',
+    api_secret: 'Y7ToNbU5cH5228pt7ER5IxIqNSQ',
+  });
+export default cloud;
